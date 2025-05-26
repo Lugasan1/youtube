2305c31def421fab5b2091d17a1f3a59ce197dee
Lucass Santosss <102385437+Lugasan1@users.noreply.github.com>
2025-05-26 00:31:54 -0300

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,165 @@
+var videos = [
+
+  {
+    video: "https://www.youtube.com/embed/VJrFh8rZ9pU", // Link de incorporação do vídeo
+    titulo: "Twist, Lick, Dunk", // Título (fica abaixo do vídeo)
+    marca: "Oreo", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/1G7WxhH/oreo-logo.png", // Logo da marca (tem que ser o link)
+    valor: 0.61 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/keOaQm6RpBg", // Link de incorporação do vídeo
+    titulo: "Ed's Heinz Ad", // Título (fica abaixo do vídeo)
+    marca: "Heinz", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/ydqQwnr/image-260nw-2385151365.webp", // Logo da marca (tem que ser o link)
+    valor: 0.65 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/HRZ2uccNVfo", // Link de incorporação do vídeo
+    titulo: "Try the NEW Colgate Active Salt toothpaste!", // Título (fica abaixo do vídeo)
+    marca: "Colgate Philippines", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/tBSP36b/unnamed.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.71 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/qIVDxL2lgN4", // Link de incorporação do vídeo
+    titulo: "Snickers Mr Bean", // Título (fica abaixo do vídeo)
+    marca: "Snickers UK", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Qnrsj9G/unnamed-1.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.54 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/u3ybWiEUaUU", // Link de incorporação do vídeo
+    titulo: "Snickers UK", // Título (fica abaixo do vídeo)
+    marca: "Ads of Brands", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Qnrsj9G/unnamed-1.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.52 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/TzPS6ELoZ1I", // Link de incorporação do vídeo
+    titulo: "Sugar Bites 30 sec TV Ad", // Título (fica abaixo do vídeo)
+    marca: "First5CoCo", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/T8LXwwq/unnamed-3.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.68 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/gXfLl3qYy0k", // Link de incorporação do vídeo
+    titulo: "Adidas – Break Free", // Título (fica abaixo do vídeo)
+    marca: "ADIDAS", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/T8LXwwq/unnamed-3.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.61 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/AbILPD3ZsZY", // Link de incorporação do vídeo
+    titulo: "Coca-Cola to Turn Up the Moment", // Título (fica abaixo do vídeo)
+    marca: "Coca-Cola", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Z1K9h6J/unnamed-4.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.49 // Valor do vídeo
+  },
+
+    {
+    video: "https://youtube.com/embed/VGa1imApfdg", // Link de incorporação do vídeo
+    titulo: "Coca-Cola® Masterpiece", // Título (fica abaixo do vídeo)
+    marca: "Coca-Cola", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Z1K9h6J/unnamed-4.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.69 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/VCPGMjCW0is", // Link de incorporação do vídeo
+    titulo: "We're Umault, a B2B video marketing agency", // Título (fica abaixo do vídeo)
+    marca: "Umault", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Nn6nTVc/unnamed-5.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.47 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/8asR_h5l7zY", // Link de incorporação do vídeo
+    titulo: "LinkedIn Corporate Branding", // Título (fica abaixo do vídeo)
+    marca: "UX Studio Asia", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/nkQRNmW/channels4-profile.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.57 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/OKo5JGjLLYY", // Link de incorporação do vídeo
+    titulo: "CEO Welcome Video 2021", // Título (fica abaixo do vídeo)
+    marca: "Hello Harold", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/nkQRNmW/channels4-profile.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.53 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/A92_B_mnO-I", // Link de incorporação do vídeo
+    titulo: "Great Ideas | Spec Ad | Post-it (2020)", // Título (fica abaixo do vídeo)
+    marca: "Filmakademie Baden-Württemberg", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/sWYvTfs/unnamed-6.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.52 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/6JFN2dac4kc", // Link de incorporação do vídeo
+    titulo: "All New Coca-Cola Spiced | Dance", // Título (fica abaixo do vídeo)
+    marca: "Coca-Cola", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Z1K9h6J/unnamed-4.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.52 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/P1zOY2wjOQQ", // Link de incorporação do vídeo
+    titulo: "Running Needs Nothing But You", // Título (fica abaixo do vídeo)
+    marca: "Adidas", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/X47YNnX/channels4-profile-1.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.62 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/g7031Ty6hqw", // Link de incorporação do vídeo
+    titulo: "Nike - Unlimited", // Título (fica abaixo do vídeo)
+    marca: "Nike", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/wCYQVGZ/unnamed-7.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.62 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/FN-apa8TKbQ", // Link de incorporação do vídeo
+    titulo: "En esta Navidad, el mejor regalo es estar juntos.", // Título (fica abaixo do vídeo)
+    marca: "Coca-Cola", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/Z1K9h6J/unnamed-4.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.48 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/iLNZdPDb5ps", // Link de incorporação do vídeo
+    titulo: "FIFA Women's World Cup 2023™", // Título (fica abaixo do vídeo)
+    marca: "Adidas", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/T8LXwwq/unnamed-3.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.58 // Valor do vídeo
+  },
+
+      {
+    video: "https://youtube.com/embed/MV2R90WFvRs", // Link de incorporação do vídeo
+    titulo: "PUB & BRAND CONTENT || Jean-Paul Gaultier", // Título (fica abaixo do vídeo)
+    marca: "Jean-Paul Gaultier", // Nome da marca (mostra abaixo do vídeo)
+    logo: "https://i.ibb.co/nkQRNmW/channels4-profile.jpg", // Logo da marca (tem que ser o link)
+    valor: 0.48 // Valor do vídeo
+  },
+
+  // Para adicionar novos vídeos, copie e cole o conteúdo abaixo removando as barras antes da linha e altere o conteúdo.
+
+  // {
+  //   video: "https://www.youtube.com/embed/VJrFh8rZ9pU",
+  //   titulo: "Twist, Lick, Dunk",
+  //   logo: "https://i.ibb.co/1G7WxhH/oreo-logo.png",
+  //   marca: "Oreo",
+  //   valor: 0.62
+  // },
+
+];
